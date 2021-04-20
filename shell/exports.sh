dfa421e4931f063f1d98e7dc811989b4cecb3746
Aytor Vicente Martínez <avicentemart@outlook.com>
2021-04-20 21:26:51 +0200

    :recycle: Refactor path exports

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,6 +1,5 @@
-export JAVA_HOME='/Library/Java/JavaVirtualMachines/amazon-corretto-15.jdk/Contents/Home'
 export GEM_HOME="$HOME/.gem"
-export GOPATH="$HOME/.go"
+export PYENV_HOME=$(pyenv root)
 
 export FZF_DEFAULT_OPTS='
   --color=pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934
@@ -11,9 +10,8 @@ export path=(
   "$HOME/bin"
   "$DOTLY_PATH/bin"
   "$DOTFILES_PATH/bin"
-  "$JAVA_HOME/bin"
   "$GEM_HOME/bin"
-  "$GOPATH/bin"
+  "$PYENV_HOME/shims"
   "$HOME/.cargo/bin"
   "/usr/local/opt/ruby/bin"
   "/usr/local/opt/python/libexec/bin"
